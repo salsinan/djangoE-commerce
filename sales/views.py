6917f8859a5283274a728599d6d62e5321f52bf2
sarah alsinan <salsinan@protonmail.com>
2022-09-05 16:43:29 -0700

    create sales and positions dataframes

--- sales/views.py
+++ sales/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Sale
 from .forms import SalesSearchForm
+import pandas as pd
 
 # Create your views here.
 def home_view(request):
+    sales_df = None
+    positions_df = None
     form = SalesSearchForm(request.POST or None)
 
     if request.method == 'POST':
@@ -12,8 +15,29 @@ def home_view(request):
         date_to = request.POST.get('date_to')
         chart_type = request.POST.get('chart_type')
 
+        sale_qs = Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
+
+        if len(sale_qs) > 0:
+            sales_df = pd.DataFrame(sale_qs.values()).to_html()
+            positions_data = []
+            for sale in sale_qs:
+                for pos in sale.get_positions():
+                    obj = {
+                        'position_id': pos.id,
+                        'product': pos.product.name,
+                        'quantity': pos.quantity,
+                        'price': pos.price,
+                    }
+                    positions_data.append(obj)
+
+            positions_df = pd.DataFrame(positions_data).to_html()
+        else:
+            print('no data')
+
     context = {
         'form': form,
+        'sales_df': sales_df,
+        'positions_df': positions_df,
     }
     return render(request, 'sales/home.html', context)
 

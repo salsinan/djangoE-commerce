47803c4ee1d41aafede03ee22d7349e11594f4d0
sarah alsinan <salsinan@protonmail.com>
2022-09-04 21:05:13 -0700

    adds crispy form to homepage

--- sales/views.py
+++ sales/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Sale
+from .forms import SalesSearchForm
 
 # Create your views here.
 def home_view(request):
+    form = SalesSearchForm(request.POST or None)
     hello = 'hello world from the view'
-    return render(request, 'sales/home.html', {'h': hello})
+    context = {
+        'hello': hello,
+        'form': form,
+    }
+    return render(request, 'sales/home.html', context)
 
 class SaleListView(ListView):
     model = Sale

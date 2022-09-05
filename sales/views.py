a6e03851b1523813977dc342cc86ba017277c823
sarah alsinan <salsinan@protonmail.com>
2022-09-04 20:47:05 -0700

    creates sales views

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Sale
 
 # Create your views here.
+def home_view(request):
+    hello = 'hello world from the view'
+    return render(request, 'sales/home.html', {'h': hello})
+
+class SaleListView(ListView):
+    model = Sale
+    template_name = 'sales/main.html'
+
+class SaleDetailView(DetailView):
+    model = Sale
+    template_name = 'sales/detail.html'

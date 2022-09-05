3fc18ca46d25167c08cbc46901357346357d6b99
sarah alsinan <salsinan@protonmail.com>
2022-09-05 06:02:38 -0700

    get data  from form

--- sales/views.py
+++ sales/views.py
@@ -6,9 +6,13 @@ from .forms import SalesSearchForm
 # Create your views here.
 def home_view(request):
     form = SalesSearchForm(request.POST or None)
-    hello = 'hello world from the view'
+
+    if request.method == 'POST':
+        date_from = request.POST.get('date_from')
+        date_to = request.POST.get('date_to')
+        chart_type = request.POST.get('chart_type')
+
     context = {
-        'hello': hello,
         'form': form,
     }
     return render(request, 'sales/home.html', context)

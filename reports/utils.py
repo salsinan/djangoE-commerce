c26116a2eaf0ab6490be4163936926d74a34c8d7
sarah alsinan <salsinan@protonmail.com>
2022-09-06 15:18:04 -0700

    enables creation of reports objects when modal is saved

---
+++ reports/utils.py
@@ -0,0 +1,13 @@
+import base64, uuid
+from django.core.files.base import ContentFile
+
+def is_ajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+
+def get_report_image(data):
+    f, str_image = data.split(';base64')
+    decoded_img = base64.b64decode(str_image)
+    img_name = str(uuid.uuid4())[:10] + '.png'
+    data = ContentFile(decoded_img, name=img_name)
+    return data
+    
--- sales/static/sales/home.js
+++ sales/static/sales/home.js
@@ -1,11 +1,41 @@
 const reportBtn = document.getElementById('report-btn');
 const img = document.getElementById('img');
 const modalBody = document.getElementById('modal-body');
+const reportForm = document.getElementById('report-form');
+
+const reportName = document.getElementById('id_name');
+const reportRemarks = document.getElementById('id_remarks');
+const csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value;
 
 if (img) {
     reportBtn.classList.remove('not-visible');
     reportBtn.addEventListener('click', ()=>{
         img.setAttribute('class', 'w-100')
         modalBody.prepend(img)
+
+        reportForm.addEventListener('submit', e=> {
+            e.preventDefault();
+            const formData = new FormData();
+            formData.append('csrfmiddlewaretoken', csrf);
+            formData.append('name', reportName.value);
+            formData.append('remarks', reportRemarks.value);
+            formData.append('image', img.src)
+
+            $.ajax({
+                type: 'POST',
+                url: '/reports/save/',
+                data: formData,
+                success: function(response){
+                    console.log("success")
+                    console.log(response)
+                },
+                error: function(error){
+                    console.log("error")
+                    console.log(error)
+                },
+                processData: false,
+                contentType: false
+            })
+        })
     });
 }
